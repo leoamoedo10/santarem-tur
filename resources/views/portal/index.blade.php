8c5a550df755494192a6b27f5794dcb8a2135cac
leoamoedo <leoamoedo10@gmail.com>
2021-10-14 16:22:42 -0300

    limpando index

--- resources/views/portal/index.blade.php
+++ resources/views/portal/index.blade.php
@@ -1,42 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.public')
 @section('content')
 
 @include('components.navbar')
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    </div>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="images/carousel-1.jpg" class="d-block w-100 heigth-img-carousel" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="images/ca.jpg" class="d-block w-100 heigth-img-carousel" alt="...">
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
-<div class="container my-3">
-    <div class="col-md-12">
-        <div class="input-group">
-            <input class="form-control" autocomplete="off" type="search" name="search" placeholder="O que você procura?">
-            <button class="btn bg-default p-3" type="submit">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                </svg>
-                Pesquisar
-            </button>
-        </div>
-    </div>
-</div>
 <div class="container">
     <div class="row py-5">
         <div class="col-md-5">
