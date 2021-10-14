8ed170cc4ea496e7ee58bd34afaae47bc3bc9f61
leoamoedo <leoamoedo10@gmail.com>
2021-10-14 16:21:58 -0300

    add novo layout

---
+++ resources/views/layouts/public.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+    <div id="app">
+        <accessibility-component></accessibility-component>
+        @yield('content')
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
