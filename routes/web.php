5341a5f0e07ae9e11ca868fc179c71c9f04767dd
leoamoedo <leoamoedo10@gmail.com>
2021-10-14 15:48:37 -0300

    ajustes na rota

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,6 @@ use App\Http\Controllers\HotelsController;
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('portal.index');
@@ -40,3 +30,7 @@ Route::prefix('eventos')
     ->group(function () {
         Route::resource('news', NewsController::class);
     });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
