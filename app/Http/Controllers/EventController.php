dd47717d45ca28d74212d5430409394afa3a6713
leoamoedo <leoamoedo10@gmail.com>
2021-10-15 15:32:06 -0300

    criando models e migrations

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class NewsController extends Controller
+class EventController extends Controller
 {
     public function index()
     {
---
+++ database/migrations/2021_10_15_133639_create_attractions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttractionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attractions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attractions');
+    }
+}
