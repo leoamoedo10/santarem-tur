75cdcdf037130cee1f249ab1b441212c6a84817a
leoamoedo <leoamoedo10@gmail.com>
2021-10-29 11:00:26 -0300

    create models

--- app/Models/Culinary.php
+++ app/Models/Culinary.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Culinary extends Model
 {
-    use HasFactory;
+    //
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    use HasFactory;
+    //
 }
