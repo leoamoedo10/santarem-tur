d8dae16aa83bebcb9cb4c6598e951490dd00f713
leoamoedo <leoamoedo10@gmail.com>
2021-10-29 10:59:39 -0300

    criando seeder de admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('543210'),
+        ]);
+    }
+}
