aa9bd2c185a414508bc5951d60633e4c17560c60
leoamoedo <leoamoedo10@gmail.com>
2021-11-29 15:54:34 -0300

    criando dashboard do admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Bem, vindo(a)!</h1>
+    <div class="container">
+        <h1>Bem vindo(a), {{Auth::user()->name}}!</h1>
+        <div class="row my-3">
+            <div class="col">
+                <a class="card" href="#">
+                    <div class="card-body">Hotéis e Pousadas</div>
+                </a>
+            </div>
+            <div class="col">
+                <a class="card" href="#">
+                    <div class="card-body">Culinária</div>
+                </a>
+            </div>
+            <div class="col">
+                <a class="card" href="#">
+                    <div class="card-body">Pontos Turísticos</div>
+                </a>
+            </div>
+            <div class="col">
+                <a class="card" href="#">
+                    <div class="card-body">Eventos</div>
+                </a>
+            </div>
+        </div>
+    </div>
 @endsection
