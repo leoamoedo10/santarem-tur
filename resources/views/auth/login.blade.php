e9804cdc5d202398229c33f3af6089c5d03b3325
leoamoedo <leoamoedo10@gmail.com>
2021-12-01 14:11:35 -0300

    implementando mensagem de error login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,15 +4,25 @@
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-4">
+            @if (count($errors) > 0)
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    @foreach($errors->all() as $error)
+                        {{ $error }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    @endforeach
+                </div>
+            @endif
             <div class="card">
-                <div class="card-header text-center alert-secondary">
-                    <h3>Autenticação</h3>
+                <div class="card-header text-center bg-container">
+                    <h3 class="text-white">Autenticação</h3>
                 </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
+                        <small class="text-muted fw-semi">Insira seus dados para acessar ao sistema</small>
+                        <hr class="my-1">
                         <div class="form-group">
                             <label for="email" class="col-form-label text-md-right">E-mail</label>
                             <div class="col">
@@ -25,7 +35,7 @@
                             </div>
 
                             <div class="col d-flex justify-content-end mt-3">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn bg-default">
                                     Login
                                 </button>
                             </div>
