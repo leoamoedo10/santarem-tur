db597a6998ecb02298e01460034c9054a1dabb57
leoamoedo <leoamoedo10@gmail.com>
2021-12-01 14:11:05 -0300

    estilizando card hotels menu admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,8 +5,15 @@
         <h1>Bem vindo(a), {{Auth::user()->name}}!</h1>
         <div class="row my-3">
             <div class="col">
-                <a class="card" href="#">
-                    <div class="card-body">Hotéis e Pousadas</div>
+                <a class="height-card card border-0 overflow-hidden shadow-sm link-default" href="{{route('admhotels.index')}}">
+                    <div class="card-body p-0 d-flex">
+                        <div class="width-icon bg-icon d-flex align-items-center justify-content-center">
+                            <i class="fas fa-hotel fa-4x"></i>
+                        </div>
+                        <div class="d-flex py-3 px-2 align-items-center">
+                            <h6 class="style-font">Hotéis e Pousadas</h6>
+                        </div>
+                    </div>
                 </a>
             </div>
             <div class="col">
