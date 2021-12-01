326ad19ccdc062b58086515d13498eaa00149bb0
leoamoedo <leoamoedo10@gmail.com>
2021-12-01 14:12:45 -0300

    listando hotels no controlador

--- app/Http/Controllers/HotelsController.php
+++ app/Http/Controllers/HotelsController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Hotel;
 use Illuminate\Http\Request;
 
 class HotelsController extends Controller
 {
     public function index()
     {
-        return view('portal.hotels.index');
+        $hotels = Hotel::all();
+
+        return view('portal.hotels.index', compact('hotels'));
+    }
+
+    public function show($id)
+    {
+        $hotel = Hotel::find($id);
+        return view('portal.hotels.show', compact('hotel'));
     }
 }
